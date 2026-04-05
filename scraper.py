259b3378ef70c3563c0deffea90cafd873792941
ArnoldRan <s2503902@gmail.com>
2026-04-06 01:11:27 +0800

    refactor: 重構成 OOP 架構

--- scraper.py
+++ scraper.py
@@ -10,131 +10,131 @@ from linebot.models import TextSendMessage
 #載入設定
 load_dotenv()
 
-def send_line_message(message):
-    token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
-    user_id = os.getenv("LINE_USER_ID")
-    print(token)
-    print(user_id)
+class LineNotifier:
+    '''負責處理通知邏輯'''
+    def __init__(self):
+        self.token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+        self.user_id = os.getenv("LINE_USER_ID")
+        self.line_bot_api = LineBotApi(self.token)
 
-    try:
-        line_bot_api = LineBotApi(token)
-        line_bot_api.push_message(user_id,TextSendMessage(text=message))
-        print("LINE 訊息已成功推播")
-    except Exception as e:
-        print(f"發送失敗:{e}")
+    def send_line_message(self,message):
+        try:
+            self.line_bot_api.push_message(self.user_id,TextSendMessage(text = message))
+            print("LINE 訊息已成功推播")
+        except Exception as e:
+            print(f"發送失敗:{e}")
 
+class PTTScraper:
+    '''負責處理爬蟲邏輯'''
+    def __init__(self,board,days):
+        self.board = board
+        self.days = days
+        self.target_date = datetime.now() - timedelta(days = days)
+        self.base_url = "https://www.ptt.cc"
+        #PTT抓取有的版需要滿１８歲以上
+        self.headers = {'cookie': 'over18=1', 'User-Agent': 'Mozilla/5.0'}
 
-def get_post_within_days(board,days):
-    
-    target_date = datetime.now() - timedelta(days = days)
+    def get_post_within_days(self):
+        url = f"{self.base_url}/bbs/{board}/index.html"
+        all_matched_posts = []
+        keep_crawling = True
 
-    #PTT抓取有的版需要滿１８歲以上
-    url = f"https://www.ptt.cc/bbs/{board}/index.html"
-    headers = {'cookie': 'over18=1', 'User-Agent': 'Mozilla/5.0'}
+        print(f"--- 開始抓取 {self.board} 版最近 {self.days} 天的文章 ---")
 
-    all_matched_posts = []
-    keep_crawling = True
+        while url and keep_crawling:
+            print(f"正在處理頁面: {url}")
+            resp = requests.get(url,headers=self.headers)
+            if resp.status_code != 200:
+                break
 
-    print(f"--- 開始抓取 {board} 版最近 {days} 天的文章 ---")
-    
-    while keep_crawling and url:
-        print(f"正在處理頁面: {url}")
-        resp = requests.get(url, headers=headers)
-        if resp.status_code != 200:
-            break
+            #解析html
+            soup = BeautifulSoup(resp.text,'html.parser')
 
-        #解析html
-        soup = BeautifulSoup(resp.text, 'html.parser')
+            sep = soup.find('div',class_='r-list-sep')
 
-        sep = soup.find('div',class_='r-list-sep')
-      
-        # 1. 抓取當前頁面的文章
-        if sep:
-            post_entries = soup.find_all_previous('div',class_= 'r-ent')
-        else:
-            post_entries = reversed(soup.find_all('div',class_= 'r-ent'))
-        
+            # 1. 抓取當前頁面的文章(置頂公告不抓)
+            if sep:
+                post_entries = sep.find_all_previous('div',class_= 'r-ent')
+            else:
+                post_entries = reversed(soup.find_all('div',class_= 'r-ent'))
 
-        for entry in post_entries:
-            title_tag = entry.find('div',class_='title').find('a')
-            date_str = entry.find('div',class_= 'date').text.strip()
+            for entry in post_entries:
+                title_tag = entry.find('div',class_='title').find('a')
+                date_str = entry.find('div',class_='date').text.strip()
+                if not title_tag: continue
 
-            if title_tag:
                 #處理跨年問題,若文章時間大於現在時間,判定為去年
                 article_date = datetime.strptime(f"{datetime.now().year}/{date_str}","%Y/%m/%d")
                 if article_date > datetime.now():
-                    article_date = article_date.replace(year= article_date.year - 1)
+                    article_date = article_date.replace(year=article_date.year -1)
 
                 # 判斷是否停止
-                if article_date < target_date:
+                if article_date < self.target_date:
                     keep_crawling = False
                     break
 
                 all_matched_posts.append({
                     'title':title_tag.text,
-                    'date': article_date.strftime("%Y-%m-%d"),
-                    'link': "https://www.ptt.cc" + title_tag['href'] 
+                    'date':article_date.strftime("%Y/%m/%d"),
+                    'link':self.base_url + title_tag['href']
                 })
-        
-        # 2. 取得上一頁連結，準備下一次迴圈
-        prev_url = soup.find('a',string ="‹ 上頁")
-        url = "https://www.ptt.cc" + prev_url['href'] if prev_url else None
-        print(url)
-        
-    return all_matched_posts
+            
+            # 2. 取得上一頁連結，準備下一次迴圈
+            prev_url = soup.find('a',string ="‹ 上頁")
+            url = self.base_url + prev_url['href'] if prev_url else None
 
-
-def save_to_csv(posts_list,filename):
-    if not posts_list:
-        print("沒有資料可以儲存!")
-        return
+        return all_matched_posts
     
-    # 將List 轉換成 DataFrame
-    df = pd.DataFrame(posts_list)
+class Save_To_Csv:
+   def __init__(self):
+        pass
+   
+   def save_to_csv(self,posts_list,filename):
+        if not posts_list:
+            print("沒有資料可以儲存!")
+            return
+        
+        #將List轉換成DataFrome,並排序
+        df = pd.DataFrame(posts_list)
+        df.sort_values(by='date',ascending=False)
 
-    # 進行排序
-    df.sort_values(by='date',ascending=False)
-
-    # 存成CSV
-    # index=False 代表不要存 Pandas 自動產生的數字編號
-    # encoding='utf-8-sig' 是為了讓 Excel 開啟時不會亂碼 (對 Mac/Windows 友善)
-    df.to_csv(filename,index = False,encoding='utf-8-sig')
-    print(f"成功儲存{len(df)}筆資料至{filename}")
+        # 存成CSV
+        # index=False 代表不要存 Pandas 自動產生的數字編號
+        # encoding='utf-8-sig' 是為了讓 Excel 開啟時不會亂碼 (對 Mac/Windows 友善)
+        df.to_csv(filename,index=False,encoding='utf-8-sig')
+        print(f"成功儲存{len(df)}筆資料至{filename}")
 
 
 
 # --- Main Code---
 if __name__ == "__main__":
     #---設定---
-    keywords=["Lakers","Warriors"]
-    days = 3
+    keywords=["Lakers","Warriors","BOX"]
+    days = 5
     board = "NBA"
 
-    # 1.抓取最近n天的文章
-    results = get_post_within_days(board,days)
-    for r in results:
-        print(f"[{r['date']}] {r['title']}")
+    # 1.初始化
+    scraper = PTTScraper(board,days)
+    notifer = LineNotifier()
+    toCsv = Save_To_Csv()
 
-    # 2.篩選關鍵字
-    matched_posts = []
-    for post in results:
-        if any(kw.lower() in post['title'].lower() for kw in keywords):
-            print(post)
-            matched_posts.append(post)
-
-    # 3.儲存至CSV
-    #results_filte = [p for p in results if "[BOX ]" in p['title']]# filter
-    if matched_posts:
-        save_to_csv(matched_posts,"ppt_csv.csv")
-
-    # 4.LINE推播
+    # 2.執行爬蟲動作
+    print(f"開始執行{board}爬蟲任務")
+    results = scraper.get_post_within_days()
     
-    if matched_posts:
-        notification_msg = f"發現{len(matched_posts)} 筆{board}板資料"
-        for p in matched_posts:
-            notification_msg += f"\n- [{p['date']}] [{p['title']} [{p['link']}]"
-            send_line_message(notification_msg)
-    
+    # 3.關鍵字篩選
+    matched = [p for p in results if any(kw.lower() in p['title'].lower() for kw in keywords)]
+
+    # 4.儲存至csv
+    toCsv.save_to_csv(matched,filename="ppt_output.csv")
+
+    # 5.發送結果
+    if matched:
+        msg = f"\n發現 {len(matched)} 筆資料：\n" + "\n".join([f"- {p['title']}\n{p['link']}" for p in matched])
+        notifer.send_line_message(msg)
     else:
-        send_line_message("今天沒有符合文章")
+        notifer.send_line_message("今天沒有符合的文章")
+    
+
+
     
